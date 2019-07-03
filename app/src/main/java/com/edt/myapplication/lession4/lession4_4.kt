a04de7368cd4257b134b4e822fa98590ee03c378
liujinan <liujinan@edreamtree.com>
2019-07-03 21:37:38 +0800

    lession4_4

--- app/src/main/java/com/edt/myapplication/lession4/lession4_4.kt
+++ app/src/main/java/com/edt/myapplication/lession4/lession4_4.kt
@@ -1,5 +1,6 @@
 package com.edt.myapplication.lession4
 
+import java.io.File
 
 /**
  * <pre>
@@ -12,20 +13,60 @@ package com.edt.myapplication.lession4
  */
 
 fun main(args: Array<String>) {
-    Payroll.allEmployees.add(Persion("张三"))
+    Payroll.allEmployees.add(Person("张三"))
     println(Payroll.allEmployees)
-    Payroll.allEmployees.add(Persion("李四"))
+    Payroll.allEmployees.add(Person("李四"))
     println(Payroll.allEmployees)
+    println(CaseInsensitiveFileComparator.compare(File("/User"), File("/user")))
+    A.bar()
 }
 
 object Payroll {
-    val allEmployees = arrayListOf<Persion>()
+    val allEmployees = arrayListOf<Person>()
 
     fun calculateSalary() {
-        for (persion in allEmployees) {
+        for (person in allEmployees) {
 
         }
     }
 }
 
-class Persion(val name: String)
+class Person(val name: String)
+
+object CaseInsensitiveFileComparator: java.util.Comparator<File> {
+    override fun compare(file1: File, file2: File): Int {
+        return file1.path.compareTo(file2.path, ignoreCase = true)
+    }
+}
+
+
+data class Person2(val name: String) {
+    object NameComparator: Comparator<Person2> {
+        override fun compare(p1: Person2, p2: Person2): Int =
+            p1.name.compareTo(p2.name)
+
+    }
+}
+
+class A {
+    companion object {
+        fun bar() {
+            println("Companion object called")
+        }
+    }
+}
+
+class User6 private constructor(val nickname: String) {
+    companion object {
+        fun newSubscribingUser(email:String) =
+                User6(email.substringBefore("@"))
+
+        fun newFacebookUser(accoutId: String) =
+                User6(getFacebookName(accoutId))
+
+        private fun getFacebookName(accoutId: String): String {
+
+            return ""
+        }
+    }
+}
